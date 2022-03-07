5d3d3b0db20c6a2ba300271abc44d3b766c45dc8
cosmonaut <rsbrandao@inf.ufrgs.br>
2022-03-06 23:02:23 -0300

    Created DataStructure for parameter

---
+++ src/DeviceParameter.py
@@ -0,0 +1,24 @@
+class DeviceParameter:
+    def __init__ (self, parameterName):
+        self.name = parameterName
+        self.value = ( -1, -1, -1)             # (min, avg, max) values aquired
+        
+        self._numSample = 0
+        self._sumSample = 0
+
+    def getParameterName(self):
+        return self.parameterName
+
+    def sumValue(self, newValue):
+        self._numSample += 1
+        self._sumSample += newValue
+
+    def setValue(self, minValue, maxValue):
+        if(self._numSample == 0):
+            print("LOG: no samples for " + self.getParameterName())
+        else
+            avgValue    = (self._sumSample)/(self._numSample)
+            self.value  = (minValue, avgValue, maxValue)
+    
+    def getValue(self):
+        return self.value
