4bfcba3424877be37bbc1525fc984315be0a0f64
cosmonaut <rsbrandao@inf.ufrgs.br>
2022-03-06 23:05:04 -0300

    Skeleton for main - without UI - skipping some steps

---
+++ src/main.py
@@ -0,0 +1,21 @@
+import Device
+
+def main():
+
+    ## login
+    ## check in db login credentials
+    ## choose wich agent to run analytics or create new profile(new agent)
+    ## create Session for the chosen agent
+    ## perform analytics
+    ## display result
+    ## store in db 
+
+    #....skiping first 3 steps
+    #device chosen somehow:
+    device = Device('Device-01', '192.168.0.106', ('MD5User', 'MD5UserPass'), ('MD5DESUser', 'MD5DESUserPass') )    
+    
+    device.createSession()
+    device.performAnalytics() # prob gonna change this function to return a tuple wich will feed some graphical engine
+
+if __name__ == '__main__':
+    main()
