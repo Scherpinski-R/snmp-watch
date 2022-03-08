e094269b3a7d9a164d92e67d2b93080f83e0ed9e
Filipe Faria Dias <35049103+FilipeFariaDias@users.noreply.github.com>
2022-03-08 18:39:04 -0300

    Indent update

--- src/main.py
+++ src/main.py
@@ -1,21 +1,26 @@
+import easysnmp
 import Device
+
 
 def main():
 
-    ## login
-    ## check in db login credentials
-    ## choose wich agent to run analytics or create new profile(new agent)
-    ## create Session for the chosen agent
-    ## perform analytics
-    ## display result
-    ## store in db 
+    # login
+    # check in db login credentials
+    # choose wich agent to run analytics or create new profile(new agent)
+    # create Session for the chosen agent
+    # perform analytics
+    # display result
+    ## store in db
 
-    #....skiping first 3 steps
-    #device chosen somehow:
-    device = Device('Device-01', '192.168.0.106', ('MD5User', 'MD5UserPass'), ('MD5DESUser', 'MD5DESUserPass') )    
-    
+    # ....skiping first 3 steps
+    # device chosen somehow:
+    device = Device('Device-01', '192.168.0.106', ('MD5User',
+                    'MD5UserPass'), ('MD5DESUser', 'MD5DESUserPass'))
+
     device.createSession()
-    device.performAnalytics() # prob gonna change this function to return a tuple wich will feed some graphical engine
+    # prob gonna change this function to return a tuple wich will feed some graphical engine
+    device.performAnalytics()
+
 
 if __name__ == '__main__':
     main()
