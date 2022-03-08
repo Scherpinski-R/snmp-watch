2732525e87381e25f38f9108346ab250a38b2cab
Filipe Faria Dias <35049103+FilipeFariaDias@users.noreply.github.com>
2022-03-08 18:40:15 -0300

    Indentation update

--- src/Device.py
+++ src/Device.py
@@ -2,42 +2,47 @@ from easysnmp import Session
 
 import DeviceParameter
 
-class Device:
-    def __init__(self, deviceAlias, deviceIp, authCred, cryptCred):
-        self.alias  = deviceAlias
-        self.ipAddr = deviceIp
-        
-        self.auth   = authCred              # (user, pass) for authenticating user
-        self.priv   = privCred              # (user, pass) for cryptographing data
-        
-        self._session = None 
 
-        #Const:
-        #TO-DO: add ifSpeed if there is time left
-        parameterNameList = ['hrStorageSize.1', 'hrStorageSize.3', 'hrSystemProcesses'] # RAM - SWAP - Number Processes
+class Device:
+    def __init__(self, deviceAlias, deviceIp, authCred, cryptCred, privCred):
+        self.alias = deviceAlias
+        self.ipAddr = deviceIp
+
+        # (user, pass) for authenticating user
+        self.auth = authCred
+        # (user, pass) for cryptographing data
+        self.priv = privCred
+
+        self._session = None
         
+
+        # Const:
+        # TO-DO: add ifSpeed if there is time left
+        # RAM - SWAP - Number Processes
+        self.parameterNameList = ['hrStorageSize.1',
+                             'hrStorageSize.3', 'hrSystemProcesses']
+
         self.deviceList = []
         self.populateParameterList()
 
     def createSession(self):
-        self._session = Session( hostname=self.ipAddr, version=3, security_level="auth_with_privacy",
-                        auth_protocol="MD5", security_username=self.auth[0], auth_password=self.auth[1]),
-                        privacy_protocol="DES", privacy_password=self.priv[1])
+        self._session = Session(hostname=self.ipAddr, version=3, security_level="auth_with_privacy", auth_protocol="MD5",
+                                security_username=self.auth[0], auth_password=self.auth[1], privacy_protocol="DES", privacy_password=self.priv[1])
 
-        if(self._session == None)
-            print("LOG: Failed to create Session") 
+        if self._session == None:
+            print("LOG: Failed to create Session")
 
-    def populateParameterList(self): 
-        for parameter in parameterNameList:
-            self.deviceList.append( DeviceParameter(parameter) )
+    def populateParameterList(self):
+        for parameter in self.parameterNameList:
+            self.deviceList.append(DeviceParameter(parameter))
 
     def performAnalytics(self):
-        if(self._session == None)
+        if self._session == None:
             print("LOG: Session not Created")
             return
 
         for parameter in self.deviceList:
-            value = self._session.get( parameter.getParameterName() )
-            
-            #do smth with value - prob compare with min, max and add to create new avg with deviceParameter methods
+            value = self._session.get(parameter.getParameterName())
+
+            # do smth with value - prob compare with min, max and add to create new avg with deviceParameter methods
             print(value)
