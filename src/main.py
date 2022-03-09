67dd3145e2d19c758c19058b7bd0ca99eced622b
cosmonaut <rsbrandao@inf.ufrgs.br>
2022-03-09 16:22:53 -0300

    Started using DB Login data

--- src/main.py
+++ src/main.py
@@ -1,10 +1,23 @@
 from Device import Device
-
+from Database import Database
 
 def main():
 
     # login
+    username = 'root'
+    password = 'toor'
     # check in db login credentials
+    db = Database('snmp-watch.db')
+
+    db.connect()
+    
+    if db.checkLogin(username, password):
+        print("Valid Login!")
+    else:
+        print("Invalid Login")
+    
+    db.close()
+
     # choose wich agent to run analytics or create new profile(new agent)
     # create Session for the chosen agent
     # perform analytics
