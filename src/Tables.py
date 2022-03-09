fea28c419c31a582fff0a03e79cc81414b87243b
Filipe Faria Dias <35049103+FilipeFariaDias@users.noreply.github.com>
2022-03-09 09:44:14 -0300

    Create tables

---
+++ src/Tables.py
@@ -0,0 +1,38 @@
+import sqlite3
+
+
+class Tables:
+    conn = sqlite3.connect('snmp.sqlite')
+
+    cursor = conn.cursor()
+
+    cursor.execute(""" 
+                CREATE TABLE IF NOT EXISTS login (
+                    user_Id INTEGER PRIMARY KEY,
+                    user TEXT not null,
+                    password TEXT
+                ); """)
+
+    cursor.execute(""" 
+                CREATE TABLE IF NOT EXISTS agent (
+                    user_Id INTEGER PRIMARY KEY,
+                    user TEXT not null,
+                    auth_user TEXT,
+                    auth_cred TEXT,
+                    priv_user TEXT,
+                    priv_cred TEXT
+                ); """)
+
+    cursor.execute(""" 
+                CREATE TABLE IF NOT EXISTS log (
+                    user_Id INTEGER PRIMARY KEY,
+                    data DATE,
+                    hora TIME,
+                    min REAL,
+                    average REAL,
+                    max REAL
+                ); """)
+    
+    conn.commit()
+    
+    conn.close()
