d6425228eee8956b9a79102bbe2f1832aaba64b3
cosmonaut <rsbrandao@inf.ufrgs.br>
2022-03-09 19:08:31 -0300

    added user methods and variables

--- src/User.py
+++ src/User.py
@@ -1,3 +1,25 @@
 class User:
     def __init__(self):
-        
+        self._userId    = -1
+        self._username   = ''
+        self._password   = ''
+
+    def getUsername(self):
+        return self._username
+
+    def setUsername(self, username):
+        # validate
+        self._username = username
+
+    def getPassword(self):
+        return self._password
+
+    def setPassword(self, password):
+        # validate
+        self._password = password
+
+    def getUserId(self):
+        return self._userId
+
+    def setUserId(self, user_id):
+        self._userId = user_id
