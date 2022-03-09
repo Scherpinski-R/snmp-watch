ae843b57bc3af2c68b59e4aa64fb58ddfdfc3fce
cosmonaut <rsbrandao@inf.ufrgs.br>
2022-03-09 18:44:24 -0300

    commented snmp calls in main to isolate DB test methods, also trying to add a new user with the methods created in Database

--- src/main.py
+++ src/main.py
@@ -4,13 +4,19 @@ from Database import Database
 def main():
 
     # login
-    username = 'root'
-    password = 'toor'
+    username = 'raphael'
+    password = '1337pass'
     # check in db login credentials
     db = Database('snmp-watch.db')
 
     db.connect()
-    
+
+    uid = db.existUsername(username)
+    if uid == -1:
+        print("Username avaible, registering...")
+
+    db.addUser(username, password)
+
     if db.checkLogin(username, password):
         print("Valid Login!")
     else:
@@ -26,11 +32,11 @@ def main():
 
     # ....skiping first 3 steps
     # device chosen somehow:
-    device = Device('Device-01', '192.168.0.106', ('MD5DESUser','The Net-SNMP Demo Password'), ('MD5User', 'The Net-SNMP Demo Password'))
+    #device = Device('Device-01', '192.168.0.106', ('MD5DESUser','The Net-SNMP Demo Password'), ('MD5User', 'The Net-SNMP Demo Password'))
 
-    device.createSession()
+    #device.createSession()
     # prob gonna change this function to return a tuple wich will feed some graphical engine
-    device.performAnalytics()
+    #device.performAnalytics()
 
 
 if __name__ == '__main__':
