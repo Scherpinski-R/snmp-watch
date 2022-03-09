571541256fe5e231383144788ffdfeac5e92e479
cosmonaut <rsbrandao@inf.ufrgs.br>
2022-03-08 22:38:06 -0300

    Changed Logic to add every Device one by one, and not feeded by a List with parameterName, and adjusted to new Interface for Device Class

--- src/DeviceParameter.py
+++ src/DeviceParameter.py
@@ -1,13 +1,38 @@
 class DeviceParameter:
-    def __init__ (self, parameterName):
-        self.name = parameterName
+    def __init__ (self, stringIdentifier, parameterName, parameterMaxName=None):
+        self.identifier = stringIdentifier  # User Friendly Name of the parameter, e.g.: RAM, SWAP
+        self.name_cur = parameterName       # Current Value of parameter
+        self.name_max = parameterMaxName    # Max Resource avaiable of parameter
+
         self.value = ( -1, -1, -1)             # (min, avg, max) values aquired
-        
+        self.maxValue = -1
+
         self._numSample = 0
         self._sumSample = 0
 
+    def setMaxValue(self, maxSystem):
+        if self.isParameterLimited():
+            self.maxValue = maxSystem
+        else:
+            print("LOG: Trying to Set a max value for ilimited parameter")
+
+    def getParameterIdentifier(self):
+        return self.identifier
+
     def getParameterName(self):
-        return self.name
+        return self.name_cur
+
+    def isParameterLimited(self):
+        if self.name_max:
+            return True
+        else:
+            return False
+
+    def getParameterMaxName(self):
+        if self.isParameterLimited():
+            return self.name_max
+        else:
+            printf("LOG: Trying to Get max value for ilimited parameter")            
 
     def sumValue(self, newValue):
         self._numSample += 1
